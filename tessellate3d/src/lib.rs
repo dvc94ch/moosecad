01b27ae500f79bde9a4648868ae5a82c8db87a17
David Craven <david@craven.ch>
2020-10-28 21:38:55 +0100

    Getting closer.

--- tessellate3d/src/lib.rs
+++ tessellate3d/src/lib.rs
@@ -29,6 +29,7 @@ pub struct IsosurfaceStuffing<'a, S: RealField> {
     dim: [usize; 3],
     mesh: Mesh<Tet4, S>,
     phi: Vec<S>,
+    cut_map: HashMap<(usize, usize), usize>,
 }
 
 impl<'a, S: Float + RealField + alga::general::RealField + From<f64>> IsosurfaceStuffing<'a, S> {
@@ -46,6 +47,7 @@ impl<'a, S: Float + RealField + alga::general::RealField + From<f64>> Isosurface
             ],
             mesh: Mesh::new(),
             phi: Default::default(),
+            cut_map: Default::default(),
         }
     }
 
@@ -122,20 +124,6 @@ impl<'a, S: Float + RealField + alga::general::RealField + From<f64>> Isosurface
         self.mesh.add_block(block);
     }
 
-    /// Interpolates the point where two coordinates intersect the boundary of the function.
-    fn cut_edge(&mut self, i1: usize, i2: usize) -> Option<Point3<S>> {
-        let zero = S::from_f64(0.0).unwrap();
-        let phi1 = self.phi[i1];
-        let phi2 = self.phi[i2];
-        if phi1 > zero && phi2 > zero || phi1 < zero && phi2 < zero {
-            return None;
-        }
-        let dphi = self.phi[i2] - self.phi[i1];
-        let dx = self.mesh.vertex(i2) - self.mesh.vertex(i1);
-        let scale = S::from_f64(2.0).unwrap() / dphi;
-        Some(Point3::from(dx * scale))
-    }
-
     /// Fix some edge crossings by warping vertices if it's admissible.
     fn warp_vertices(&mut self) {
         //assert!(self.warp_threshold >= 0.0 && self.warp_threshold <= 0.5);
@@ -184,8 +172,152 @@ impl<'a, S: Float + RealField + alga::general::RealField + From<f64>> Isosurface
         }
     }
 
+    /// Interpolates the point where two coordinates intersect the boundary of the function.
+    fn cut_edge(&mut self, a: usize, b: usize) -> usize {
+        let zero = S::from_f64(0.0).unwrap();
+        let one = S::from_f64(1.0).unwrap();
+        debug_assert!(
+            self.phi[a] > zero && self.phi[b] < zero || self.phi[a] < zero && self.phi[b] > zero
+        );
+        let edge = (usize::min(a, b), usize::max(a, b));
+        if let Some(i) = self.cut_map.get(&edge) {
+            return *i;
+        }
+        let alpha = self.phi[a] / self.phi[a] - self.phi[b];
+        let vertex =
+            self.mesh.vertex(a).coords * (one - alpha) + self.mesh.vertex(b).coords * alpha;
+        let index = self.mesh.vertices().len();
+        self.mesh.add_vertex(Point3::from(vertex));
+        self.phi.push(zero);
+        self.cut_map.insert(edge, index);
+        index
+    }
+
     /// Find all tets with vertices where a corner has vphi > 0 and trim them back.
-    fn trim_spikes(&mut self) {}
+    fn trim_spikes(&mut self) {
+        let zero = S::from_f64(0.0).unwrap();
+        let mut new_tets = Vec::new();
+        for bi in 0..self.mesh.blocks().len() {
+            let mut ti = 0;
+            while ti < self.mesh.block(bi).elems().len() {
+                let mut a = self.mesh.block(bi).elem(ti).node(0) as usize;
+                let mut b = self.mesh.block(bi).elem(ti).node(1) as usize;
+                let mut c = self.mesh.block(bi).elem(ti).node(2) as usize;
+                let mut d = self.mesh.block(bi).elem(ti).node(3) as usize;
+                if self.phi[a] <= zero
+                    && self.phi[b] <= zero
+                    && self.phi[c] <= zero
+                    && self.phi[d] <= zero
+                {
+                    ti += 1;
+                    continue;
+                }
+
+                let mut flipped = false;
+                if self.phi[a] < self.phi[b] || (self.phi[a] == self.phi[b] && a < b) {
+                    std::mem::swap(&mut a, &mut b);
+                    flipped = !flipped;
+                }
+                if self.phi[c] < self.phi[d] || (self.phi[c] == self.phi[d] && c < d) {
+                    std::mem::swap(&mut c, &mut d);
+                    flipped = !flipped;
+                }
+                if self.phi[a] < self.phi[c] || (self.phi[a] == self.phi[c] && a < c) {
+                    std::mem::swap(&mut a, &mut c);
+                    flipped = !flipped;
+                }
+                if self.phi[b] < self.phi[d] || (self.phi[b] == self.phi[d] && b < d) {
+                    std::mem::swap(&mut b, &mut d);
+                    flipped = !flipped;
+                }
+                if self.phi[b] < self.phi[c] || (self.phi[b] == self.phi[c] && b < c) {
+                    std::mem::swap(&mut b, &mut c);
+                    flipped = !flipped;
+                }
+
+                debug_assert!(
+                    self.phi[a] >= self.phi[b]
+                        && self.phi[b] >= self.phi[c]
+                        && self.phi[c] >= self.phi[d]
+                );
+                debug_assert!(self.phi[a] > zero);
+                debug_assert!(self.phi[d] <= zero);
+
+                if self.phi[d] == zero {
+                    self.mesh.block_mut(bi).swap_remove(ti);
+                    break;
+                } else if self.phi[b] == zero && self.phi[c] == zero {
+                    let e = self.cut_edge(a, d);
+                    *self.mesh.block_mut(bi).elem_mut(ti) = if flipped {
+                        Tet4::new([b as u32, e as u32, c as u32, d as u32])
+                    } else {
+                        Tet4::new([e as u32, b as u32, c as u32, d as u32])
+                    };
+                } else if self.phi[b] == zero {
+                    let e = self.cut_edge(a, c);
+                    let f = self.cut_edge(a, d);
+                    *self.mesh.block_mut(bi).elem_mut(ti) = if flipped {
+                        new_tets.push(Tet4::new([b as u32, e as u32, f as u32, d as u32]));
+                        Tet4::new([b as u32, e as u32, c as u32, d as u32])
+                    } else {
+                        new_tets.push(Tet4::new([e as u32, b as u32, f as u32, d as u32]));
+                        Tet4::new([e as u32, b as u32, c as u32, d as u32])
+                    };
+                } else if self.phi[c] == zero {
+                    let e = self.cut_edge(a, d);
+                    let f = self.cut_edge(b, d);
+                    *self.mesh.block_mut(bi).elem_mut(ti) = if flipped {
+                        Tet4::new([f as u32, e as u32, c as u32, d as u32])
+                    } else {
+                        Tet4::new([e as u32, f as u32, c as u32, d as u32])
+                    };
+                } else if self.phi[b] > zero && self.phi[c] < zero {
+                    let e = self.cut_edge(a, c);
+                    let f = self.cut_edge(a, d);
+                    let g = self.cut_edge(b, c);
+                    let h = self.cut_edge(b, d);
+                    *self.mesh.block_mut(bi).elem_mut(ti) = if flipped {
+                        new_tets.push(Tet4::new([f as u32, e as u32, g as u32, d as u32]));
+                        new_tets.push(Tet4::new([g as u32, f as u32, h as u32, d as u32]));
+                        Tet4::new([g as u32, e as u32, c as u32, d as u32])
+                    } else {
+                        new_tets.push(Tet4::new([e as u32, f as u32, g as u32, d as u32]));
+                        new_tets.push(Tet4::new([f as u32, g as u32, h as u32, d as u32]));
+                        Tet4::new([e as u32, g as u32, c as u32, d as u32])
+                    }
+                } else if self.phi[b] > zero && self.phi[c] > zero {
+                    let e = self.cut_edge(a, d);
+                    let f = self.cut_edge(b, d);
+                    let g = self.cut_edge(c, d);
+                    *self.mesh.block_mut(bi).elem_mut(ti) = if flipped {
+                        Tet4::new([f as u32, e as u32, g as u32, d as u32])
+                    } else {
+                        Tet4::new([f as u32, e as u32, g as u32, d as u32])
+                    }
+                } else if self.phi[b] < zero && self.phi[c] < zero {
+                    let e = self.cut_edge(a, b);
+                    let f = self.cut_edge(a, c);
+                    let g = self.cut_edge(a, d);
+                    *self.mesh.block_mut(bi).elem_mut(ti) = if flipped {
+                        new_tets.push(Tet4::new([g as u32, e as u32, c as u32, d as u32]));
+                        new_tets.push(Tet4::new([f as u32, e as u32, c as u32, g as u32]));
+                        Tet4::new([b as u32, e as u32, c as u32, d as u32])
+                    } else {
+                        new_tets.push(Tet4::new([g as u32, e as u32, c as u32, d as u32]));
+                        new_tets.push(Tet4::new([f as u32, e as u32, c as u32, g as u32]));
+                        Tet4::new([e as u32, b as u32, c as u32, d as u32])
+                    }
+                } else {
+                    panic!("unconsidered case");
+                }
+                ti += 1;
+            }
+            let block = self.mesh.block_mut(bi);
+            for tet in new_tets.drain(..) {
+                block.add_elem(tet);
+            }
+        }
+    }
 
     /// Remove tets with corners where phi = 0 but are outside the volume.
     fn remove_exterior_tets(&mut self) {
@@ -202,7 +334,7 @@ impl<'a, S: Float + RealField + alga::general::RealField + From<f64>> Isosurface
                 let phib = self.phi[b];
                 let phic = self.phi[c];
                 let phid = self.phi[d];
-                // assert!(phia <= zero && phib <= zero && phic <= zero && phid <= zero);
+                debug_assert!(phia <= zero && phib <= zero && phic <= zero && phid <= zero);
                 if phia == zero && phib == zero && phic == zero && phid == zero {
                     let p = self.mesh.vertex(a).coords
                         + self.mesh.vertex(b).coords
